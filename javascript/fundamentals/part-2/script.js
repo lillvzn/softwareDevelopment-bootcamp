ce1ea6e40e5bfbd78231e674ff4b6dd7da15a4c8
Punith <punith.p@icloud.com>
2021-10-08 13:55:36 +0530

    learn js fundamentals

--- javascript/fundamentals/part-2/script.js
+++ javascript/fundamentals/part-2/script.js
@@ -21,7 +21,7 @@ if (hasDriverLicense) console.log("Drive");
 console.log("<---- Functions ---->");
 
 function logger() {
-  // function buddy
+  // function body
   console.log("My name is!");
 }
 
@@ -56,7 +56,6 @@ const ageUsingExpression = function (birthYear) {
 };
 console.log(ageUsingExpression(1997));
 
-*/
 
 // <----------- Arrow functions ----------->
 
@@ -78,3 +77,92 @@ const additionFunction = (num1, num2) => {
   return addition;
 };
 console.log(additionFunction(4001, 79799));
+
+
+
+// <----------- Function calling other functions ----------->
+
+function cutFruits(fruit) {
+  return fruit * 4;
+}
+function fruitJuice(apples, oranges) {
+  const applePieces = cutFruits(apples);
+  const orangePieces = cutFruits(oranges);
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitJuice(3, 5));
+
+
+
+// CHALLENGE
+
+const calcAvg = (scores) => {
+  const totalScore = scores.reduce((prev, next) => prev + next, 0);
+  return totalScore / scores.length;
+};
+
+function checkWinner(dolphinScores, koalaScores) {
+  const dolphinAvg = calcAvg(dolphinScores);
+  const koalaAvg = calcAvg(koalaScores);
+
+  if (dolphinAvg >= koalaAvg * 2)
+    return `Dolphins win! (${dolphinAvg} v. ${koalaAvg})`;
+  else if (koalaAvg >= dolphinAvg * 2)
+    return `Koalas win! (${koalaAvg} v. ${dolphinAvg})`;
+  else
+    return `No team has won! (Koala: ${koalaAvg} v. Dolphins: ${dolphinAvg})`;
+}
+
+// // TEST DATA 1
+// const dolphinScores = [44, 23, 71];
+// const koalaScores = [65, 54, 49];
+
+// TEST DATA 2
+const dolphinScores = [85, 54, 41];
+const koalaScores = [23, 34, 27];
+
+console.log(checkWinner(dolphinScores, koalaScores));
+
+
+
+// <----------- Introduction to arrays ----------->
+
+console.log("<---- Arrays ---->");
+
+const friends = ["Cloe", "Ana", "Talia"];
+// or const friendsAlternative = new Array("Cloe", "Ana", "Kortney");
+
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jacob";
+
+// basic operations
+
+friends.push("Simon"); // add to end
+friends.unshift("Sandy"); // add to start
+friends.pop(); // pop end
+friends.shift(); // pop start
+console.log(friends);
+console.log(friends.indexOf("Ana")); // index
+console.log(friends.includes("Jacob")); // if present, returns true (ES6) - no coersion
+
+*/
+
+// CHALLENGE
+
+const calcTip = (bill) => {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+// console.log(calcTip(400));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(`Bills: ${bills}`);
+console.log(`Tips: ${tips}`);
+console.log(`Total: ${total}`);
