816e1a0de021dedaf75654b2d9eb4c4d38f003e2
Punith <punith.p@icloud.com>
2021-10-13 19:09:40 +0530

    learn dom manipulation

--- javascript/DOM&events-fundamentals/p3_pig-game/script.js
+++ javascript/DOM&events-fundamentals/p3_pig-game/script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const reset = document.querySelector(".btn--new");
-
 const score = document.querySelectorAll(".score");
 let scoreOne = document.getElementById("score--0");
 let scoreTwo = document.getElementById("score--1");
@@ -11,6 +9,7 @@ const playerTwo = document.querySelector(".player--1");
 
 const rollBtn = document.querySelector(".btn--roll");
 const holdBtn = document.querySelector(".btn--hold");
+const resetBtn = document.querySelector(".btn--new");
 
 const dice = document.querySelector(".dice");
 const diceToRoll = document.getElementById("roll-dice");
@@ -19,56 +18,104 @@ const currentScore = document.querySelectorAll(".current-score");
 const currentOne = document.getElementById("current--0");
 const currentTwo = document.getElementById("current--1");
 
-let randomDice = Math.trunc(Math.random() * 6) + 1;
 let p1CurScore = 0;
 let p2CurScore = 0;
 let p1TotalScore = 0;
 let p2TotalScore = 0;
 
-reset.addEventListener("click", function () {
+const checkPlayerActive = function (whichPlayer) {
+  return whichPlayer.classList.contains("player--active");
+};
+
+const togglePlayerActive = function (whichPlayer) {
+  return whichPlayer.classList.toggle("player--active");
+};
+
+const winner = function (whichPlayer) {
+  return whichPlayer.classList.add("player--winner");
+};
+
+const removeWinner = function (whichPlayer) {
+  return whichPlayer.classList.remove("player--winner");
+};
+
+const disableBtn = function (value) {
+  const rollBtnVal = (rollBtn.disabled = value);
+  const holdBtnVal = (holdBtn.disabled = value);
+  return rollBtnVal, holdBtnVal;
+};
+
+// reset play
+resetBtn.addEventListener("click", function () {
   for (let i = 0; i < score.length; i++) {
     score[i].textContent = 0;
     currentScore[i].textContent = 0;
   }
+  p1TotalScore = 0;
+  p2TotalScore = 0;
   playerOne.classList.add("player--active");
+  playerTwo.classList.remove("player--active");
   dice.classList.add("hidden");
+  removeWinner(playerOne);
+  removeWinner(playerTwo);
+  disableBtn(false);
 });
 
+// Roll the dice
 rollBtn.addEventListener("click", function () {
+  const randomDice = Math.trunc(Math.random() * 6) + 1;
   dice.classList.remove("hidden");
-  randomDice = Math.trunc(Math.random() * 6) + 1;
-  diceToRoll.src = `dice-${randomDice}.png`;
+  dice.src = `dice-${randomDice}.png`;
 
   if (randomDice === 1) {
-    if (playerOne) {
+    if (checkPlayerActive(playerOne)) {
       currentOne.textContent = 0;
       p1CurScore = 0;
-      playerOne.classList.remove("player--active");
-      playerTwo.classList.add("player--active");
-    } else if (playerTwo) {
+      togglePlayerActive(playerOne);
+      togglePlayerActive(playerTwo);
+    } else if (checkPlayerActive(playerTwo)) {
       currentTwo.textContent = 0;
       p2CurScore = 0;
-      playerOne.classList.add("player--active");
+      togglePlayerActive(playerTwo);
+      togglePlayerActive(playerOne);
     }
   } else if (randomDice !== 1) {
-    if (playerOne) {
+    if (checkPlayerActive(playerOne)) {
       p1CurScore += randomDice;
       currentOne.textContent = p1CurScore;
-    } else if (playerTwo) {
+    } else if (checkPlayerActive(playerTwo)) {
       p2CurScore += randomDice;
       currentTwo.textContent = p2CurScore;
     }
   }
 });
 
+// hold value
 holdBtn.addEventListener("click", function () {
-  if (playerOne) {
+  if (checkPlayerActive(playerOne)) {
     p1TotalScore += p1CurScore;
     scoreOne.textContent = p1TotalScore;
     currentOne.textContent = 0;
-  } else if (playerTwo) {
+    p1CurScore = 0;
+    togglePlayerActive(playerOne);
+    togglePlayerActive(playerTwo);
+  } else if (checkPlayerActive(playerTwo)) {
     p2TotalScore += p2CurScore;
     scoreTwo.textContent = p2TotalScore;
     currentTwo.textContent = 0;
+    p2CurScore = 0;
+    togglePlayerActive(playerTwo);
+    togglePlayerActive(playerOne);
+  }
+  if (p1TotalScore >= 100) {
+    winner(playerOne);
+    currentOne.textContent = p1CurScore;
+    dice.classList.add("hidden");
+    disableBtn(true);
+  } else if (p2TotalScore >= 100) {
+    winner(playerTwo);
+    currentTwo.textContent = p2CurScore;
+    dice.classList.add("hidden");
+    disableBtn(true);
   }
 });
