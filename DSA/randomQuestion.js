0cd244d3d79e530a5b8731bd218478925ec00fbf
Punith <punith.p@icloud.com>
2021-11-14 13:10:57 +0530

    data structures random question

---
+++ DSA/randomQuestion.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+
+Given 2 arrays, create a function that let's the user know whether two arrays contain any common items (return true or false)
+
+Example 1:
+arr1 = ['a', 'b', 'c', 'z'];
+arr2 = ['m', 'n', 's'];
+-----> return false
+
+Example 2:
+arr1 = ['a', 'b', 'c', 'z'];
+arr2 = ['m', 'z', 'n'];
+-----> return true
+
+*/
+
+// points
+// 2 arrays, if there's an element common in both arrays, return true, else false
+
+// Questions
+// are the elements repeated?
+// how big can the array get? (constraints)
+
+const commonEl = function (arr1, arr2) {};
